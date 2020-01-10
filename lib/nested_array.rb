dd08d862fba0a21dae71589f1f5f7c8d4f8b57b5
Rick Glascock <rixong@gmail.com>
2020-01-10 02:33:33 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,7 +30,7 @@ end
 
 def sorted_matrix
   
-  array = [%w[Grapefruit, Pineapple, Oranges, Watermelon, Eggplant], %w[Strawberries, Potatoes, Grapes, Avocadoes, Asparagus]]
+  array = [%w[Grapefruit, Pineapple, Oranges, Watermelon, Eggplant].sort, %w[Strawberries, Potatoes, Grapes, Avocadoes, Asparagus].sort]
   
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
